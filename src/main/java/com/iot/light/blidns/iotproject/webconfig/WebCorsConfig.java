661b2d3c887a46a3f431595c14391aec0a1de286
Vladyslav <vladyslav.todosiuk@student.tuke.sk>
2023-01-10 20:01:07 +0100

    add

---
+++ src/main/java/com/iot/light/blidns/iotproject/webconfig/WebCorsConfig.java
@@ -0,0 +1,17 @@
+package com.iot.light.blidns.iotproject.webconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST");
+    }
+}
